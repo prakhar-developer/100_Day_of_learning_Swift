d7d5da2d49ec135585541c4fd77d71e39d32f4a7
Prakhar Srivastava | Developer <prakhar_developer@outlook.com>
2024-05-25 14:18:16 +0530

    Add files via upload

---
+++ main.swift
@@ -0,0 +1,91 @@
+//
+//  main.swift
+//  Day1_of_Swift_programming
+//
+//  Created by STUDENT on 25/05/24.
+//
+
+import Foundation
+
+print("Hello, World!")
+
+var myVariable = 42
+myVariable = 50
+let myConstant = 42
+let implicitInteger = 70
+let implicitDouble = 70.0
+let explicitDouble: Double = 70
+print(myConstant)
+print(implicitDouble)
+print(implicitInteger)
+print(implicitInteger)
+print(explicitDouble)
+
+
+
+let label = "The width is "
+let width = 94
+let widthLabel = label + String(width)
+print(widthLabel)
+
+
+
+
+
+let apples = 3
+let oranges = 5
+let appleSummary = "I have \(apples) apples."
+let fruitSummary = "I have \(apples + oranges) pieces of fruit."
+print(appleSummary)
+print(fruitSummary)
+
+
+
+
+
+let quotation = """
+        Even though there's whitespace to the left,
+        the actual lines aren't indented.
+            Except for this line.
+        Double quotes (") can appear without being escaped.
+
+        I still have \(apples + oranges) pieces of fruit.
+        """
+
+print(quotation)
+
+
+
+var fruits = ["strawberries", "limes", "tangerines"]
+print(fruits)
+fruits[1] = "grapes"   //limes ki jgh grapes aa jayega
+print(fruits)
+
+var occupations = [
+    "Malcolm": "Captain",
+    "Kaylee": "Mechanic",
+ ]
+occupations["Jayne"] = "Public Relations"
+
+fruits.append("blueberries")
+print(fruits)
+print(occupations)
+
+
+
+//if else wle concept
+
+
+let individualScores = [75, 43, 103, 87, 12]
+var teamScore = 0
+for score in individualScores {
+    if score > 50 {
+        teamScore += 3
+    } else {
+        teamScore += 1
+    }
+}
+print(teamScore)
+
+
+

1b25b743ae0c4e6dbc3bf57a15876753fbbc3acd
Prakhar Srivastava | Developer <prakhar_developer@outlook.com>
2024-05-30 15:00:30 +0530

    Add files via upload

--- main.swift
+++ main.swift
@@ -1,91 +1,89 @@
 //
 //  main.swift
-//  Day1_of_Swift_programming
+//  Day2_of_swift_programming
 //
-//  Created by STUDENT on 25/05/24.
+//  Created by STUDENT on 30/05/24.
 //
 
 import Foundation
 
-print("Hello, World!")
-
-var myVariable = 42
-myVariable = 50
-let myConstant = 42
-let implicitInteger = 70
-let implicitDouble = 70.0
-let explicitDouble: Double = 70
-print(myConstant)
-print(implicitDouble)
-print(implicitInteger)
-print(implicitInteger)
-print(explicitDouble)
+var optionalString: String? = "Hello"
+print(optionalString == nil)
+// Prints "false"
 
 
-
-let label = "The width is "
-let width = 94
-let widthLabel = label + String(width)
-print(widthLabel)
-
-
-
+var optionalName: String? = "John Appleseed"
+var greeting = "Hello!"
+if let name = optionalName {
+    greeting = "Hello, \(name)"
+}
+print(greeting)
 
 
-let apples = 3
-let oranges = 5
-let appleSummary = "I have \(apples) apples."
-let fruitSummary = "I have \(apples + oranges) pieces of fruit."
-print(appleSummary)
-print(fruitSummary)
+let vegetable = "red pepper"
+switch vegetable {
+case "celery":
+    print("hi")
+case "cucumber","watercress":
+    print("hello")
+case let x where x.hasSuffix("pepper"):
+    print("is it spicy \(x)?")
+default:
+    print("Everything taste good")
 
+}
 
+let intrestingNumber = [
+    "Prime": [2,3,5,7,11,13,17],
+    "Fibonacci":[1,1,2,3,5,8],
+    "Square":[1,4,9,16,25],
+]
 
-
-
-let quotation = """
-        Even though there's whitespace to the left,
-        the actual lines aren't indented.
-            Except for this line.
-        Double quotes (") can appear without being escaped.
-
-        I still have \(apples + oranges) pieces of fruit.
-        """
-
-print(quotation)
-
-
-
-var fruits = ["strawberries", "limes", "tangerines"]
-print(fruits)
-fruits[1] = "grapes"   //limes ki jgh grapes aa jayega
-print(fruits)
-
-var occupations = [
-    "Malcolm": "Captain",
-    "Kaylee": "Mechanic",
- ]
-occupations["Jayne"] = "Public Relations"
-
-fruits.append("blueberries")
-print(fruits)
-print(occupations)
-
-
-
-//if else wle concept
-
-
-let individualScores = [75, 43, 103, 87, 12]
-var teamScore = 0
-for score in individualScores {
-    if score > 50 {
-        teamScore += 3
-    } else {
-        teamScore += 1
+var largest = 0
+for (_,numbers) in intrestingNumber {
+    for number in numbers{
+        if number > largest{
+            largest = number
+        }
     }
 }
-print(teamScore)
+print(largest)
+
+var n = 2
+while n < 100 {
+    n *= 2
+}
+print(n)
+// Prints "128"
+
+
+var m = 2
+repeat {
+    m *= 2
+} while m < 100
+print(m)
+
+var total = 0
+for i in 0..<4 {
+    total += i
+}
+print(total)
 
 
 
+//function
+
+
+// function with two parameters
+func addNumbers(num1: Int, num2: Int) {
+  var sum = num1 + num2
+  print("Sum: ",sum)
+}
+
+// function with no parameter
+addNumbers(num1: 5, num2: 7)
+
+func greet(person: String, strength: Int){
+    return (Hello \(person), total strength \(strength))
+}
+greet(person: "Prakhar", strength: 20)
